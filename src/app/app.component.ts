8137e26da25d8d3ee4b6642866c6a380cb25a231
DieguitoLOL <diegoabdalaflorez@gmail.com>
2024-04-13 20:47:58 -0500

    Maquetacion de la pagina act-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ActAcronimosComponent } from './components/games/act-acronimos/act-acronimos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ActAcronimosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
