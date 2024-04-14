767cf2abbb795d7363ef23ce89e212fb1c251410
Gabrielrmr <murallas512bv@gmail.com>
2024-04-13 23:34:45 -0500

    MaquetadoVerbosFraaciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ActAcronimosComponent } from './components/games/act-acronimos/act-acronimos.component';
+import { ActVerbosFracialesComponent } from './components/games/act-verbos-fraciales/act-verbos-fraciales.component';
+import { ActPalabrasVerbosFracialesComponent } from './components/games/act-palabras-verbos-fraciales/act-palabras-verbos-fraciales.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ActAcronimosComponent],
+  imports: [CommonModule, RouterOutlet, ActAcronimosComponent,ActVerbosFracialesComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
