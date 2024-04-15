e6c430cd6700aab50ddbeba5a117088baad908bf
daya-pao <dayana06052002@gmail.com>
2024-04-14 21:03:42 -0500

    maquetacion lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ActAcronimosComponent } from './components/games/act-acronimos/act-acronimos.component';
 import { ActVerbosFracialesComponent } from './components/games/act-verbos-fraciales/act-verbos-fraciales.component';
+import { ActPalabrasComponent } from './components/games/act-palabras/act-palabras.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ActAcronimosComponent,ActVerbosFracialesComponent ],
+  imports: [CommonModule, RouterOutlet, ActAcronimosComponent,ActVerbosFracialesComponent,ActPalabrasComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
