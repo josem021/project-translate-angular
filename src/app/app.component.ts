6af28d60ded6107d1307879acaf3a995e39793e9
JoseM021 <122744449+JoseM021@users.noreply.github.com>
2024-04-16 01:46:36 -0500

    maquetado basico, pagina home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { ActAcronimosComponent } from './components/games/act-acronimos/act-acronimos.component';
 import { ActVerbosFracialesComponent } from './components/games/act-verbos-fraciales/act-verbos-fraciales.component';
 import { ActPalabrasComponent } from './components/games/act-palabras/act-palabras.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ActAcronimosComponent,ActVerbosFracialesComponent,ActPalabrasComponent ],
+  imports: [CommonModule, RouterOutlet, ActAcronimosComponent,ActVerbosFracialesComponent,ActPalabrasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ActAcronimosComponent } from './components/games/act-acronimos/act-acronimos.component';
+import { ActPalabrasComponent } from './components/games/act-palabras/act-palabras.component';
+import { ActPalabrasVerbosFracialesComponent } from './components/games/act-palabras-verbos-fraciales/act-palabras-verbos-fraciales.component';
+import { ActVerbosFracialesComponent } from './components/games/act-verbos-fraciales/act-verbos-fraciales.component';
 
 export const routes: Routes = [
+    {path: 'act-acronimos', component: ActAcronimosComponent},
+    {path: 'act-palabras', component: ActPalabrasComponent},
+    {path: 'act-palabras-verbos-fraciales', component: ActPalabrasVerbosFracialesComponent},
+    {path: 'act-verbos-fraciales', component: ActVerbosFracialesComponent },
     {path: 'home', component: HomeComponent},
     {path: '**', redirectTo: 'home'}
 ];
