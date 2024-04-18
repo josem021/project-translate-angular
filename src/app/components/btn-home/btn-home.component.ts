d3f4af3fb98f9b180b6ebce984df0c77fbdc9177
fafi-svg <rfaelsolanob@gmail.com>
2024-04-18 07:04:19 -0500

    Create btn-home.component

---
+++ src/app/components/btn-home/btn-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-btn-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './btn-home.component.html',
+  styleUrl: './btn-home.component.css'
+})
+export class BtnHomeComponent {
+
+}
